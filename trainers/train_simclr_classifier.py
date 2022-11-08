b864d6bb90b22f45256ce043e6ebe13ec10985d8
Tom <tom.smeets1@hotmail.com>
2022-11-08 22:22:14 +0100

    Finished most of code for logistic regression on top of simclr

--- trainers/train_simclr_classifier.py
+++ trainers/train_simclr_classifier.py
@@ -1,18 +1,76 @@
 import os
 
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+from tqdm.notebook import tqdm
+
+import constants
+from datasets.SHHS_dataset_timeonly import SHHS_dataset_1, EEGdataModule
+
 from models.logistic_regression import LogisticRegression
+from models.simclr_model import CNNmodel_SimCLR
 import torch
+import torch.utils.data as data
 from copy import deepcopy
 import torch.nn as nn
+import matplotlib.pyplot as plt
 
 
 def train_simclr_classifier(args, device):
-    pass
+    dict_args = vars(args)
+    pretrained_filename = os.path.join(dict_args['CHECKPOINT_PATH'], "simclr01.ckpt") # load trained simclr model
+
+    if os.path.isfile(pretrained_filename):
+        print(f"Found pretrained model at {pretrained_filename}, loading...")
+    else:
+        print(f"No pretrained simclr model found at {pretrained_filename}, exiting ...")
+        exit(1)
+    model = CNNmodel_SimCLR.load_from_checkpoint(pretrained_filename)
 
 
-def train_logreg(batch_size, train_feats_data, test_feats_data, model_suffix, max_epochs=100, **kwargs):
-    trainer = pl.Trainer(default_root_dir=os.path.join(CHECKPOINT_PATH, "LogisticRegression"),
+    results = {}
+    for num_patients_per_label in [1, 5, 10, 100]:
+        dm = EEGdataModule(DATA_PATH=args.DATA_PATH,
+                           batch_size=64,
+                           data_split=[3, 1],
+                           num_patients_train=num_patients_per_label,
+                           num_patients_test=num_patients_per_label,
+                           num_workers=args.NUM_WORKERS
+                           )
+
+        sub_train_set = prepare_data_features(model, dm.train_dataloader(), device, **dict_args)
+        test_feats_simclr = prepare_data_features(model, dm.test_dataloader(), device, **dict_args)
+
+        _, small_set_results = train_logreg(batch_size=64,
+                                            train_feats_data=sub_train_set,
+                                            test_feats_data=test_feats_simclr,
+                                            model_suffix=num_patients_per_label,
+                                            feature_dim=int(constants.SLEEP_EPOCH_SIZE/8 * args.model_hparams['conv_filters'][-1]),
+                                            num_classes=10,
+                                            lr=1e-3,
+                                            weight_decay=1e-3)
+        results[num_patients_per_label] = small_set_results
+
+    dataset_sizes = sorted([k for k in results])
+    test_scores = [results[k]["test"] for k in dataset_sizes]
+
+    fig = plt.figure(figsize=(6, 4))
+    plt.plot(dataset_sizes, test_scores, '--', color="#000", marker="*", markeredgecolor="#000", markerfacecolor="y",
+             markersize=16)
+    plt.xscale("log")
+    plt.xticks(dataset_sizes, labels=dataset_sizes)
+    plt.title("STL10 classification over dataset size", fontsize=14)
+    plt.xlabel("Number of images per class")
+    plt.ylabel("Test accuracy")
+    plt.minorticks_off()
+    plt.show()
+
+    for k, score in zip(dataset_sizes, test_scores):
+        print(f'Test accuracy for {k:3d} patients: {100 * score:4.2f}%')
+
+
+def train_logreg(batch_size, train_feats_data, test_feats_data, model_suffix, device, max_epochs=100, **kwargs):
+    trainer = pl.Trainer(default_root_dir=os.path.join(kwargs['CHECKPOINT_PATH'], "LogisticRegression"),
                          accelerator="gpu" if str(device).startswith("cuda") else "cpu",
                          devices=1,
                          max_epochs=max_epochs,
@@ -29,7 +87,7 @@ def train_logreg(batch_size, train_feats_data, test_feats_data, model_suffix, ma
                                   drop_last=False, pin_memory=True, num_workers=0)
 
     # Check whether pretrained model exists. If yes, load it and skip training
-    pretrained_filename = os.path.join(CHECKPOINT_PATH, f"LogisticRegression_{model_suffix}.ckpt")
+    pretrained_filename = os.path.join(kwargs['CHECKPOINT_PATH'], f"LogisticRegression_{model_suffix}.ckpt")
     if os.path.isfile(pretrained_filename):
         print(f"Found pretrained model at {pretrained_filename}, loading...")
         model = LogisticRegression.load_from_checkpoint(pretrained_filename)
@@ -46,25 +104,19 @@ def train_logreg(batch_size, train_feats_data, test_feats_data, model_suffix, ma
 
     return model, result
 
-
-def load_model():
-    pass
-
-
 @torch.no_grad()
-def prepare_data_features(model, dataset):
+def prepare_data_features(model, data_loader, device, **kwargs):
     # Prepare model
-    network = deepcopy(model.convnet)
-    network.fc = nn.Identity()  # Removing projection head g(.)
+    network = deepcopy(model.f)
+    network.g = nn.Identity()  # Removing projection head g(.)
     network.eval()
     network.to(device)
 
     # Encode all images
-    data_loader = data.DataLoader(dataset, batch_size=64, num_workers=NUM_WORKERS, shuffle=False, drop_last=False)
     feats, labels = [], []
-    for batch_imgs, batch_labels in tqdm(data_loader):
-        batch_imgs = batch_imgs.to(device)
-        batch_feats = network(batch_imgs)
+    for batch_inputs, batch_labels in tqdm(data_loader):
+        batch_inputs = batch_inputs.to(device)
+        batch_feats = network(batch_inputs)
         feats.append(batch_feats.detach().cpu())
         labels.append(batch_labels)
 
