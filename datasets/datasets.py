Fixed normalization

datasets/datasets.py
@@ -96,12 +96,10 @@ class SHHS_dataset_STFT(torch.utils.data.Dataset):
             try:
                 f = h5py.File(datapoint, 'r')
                 x2 = torch.as_tensor(np.array(f.get("X2"))).permute(2, 1, 0)
-                b, t, fr = x2.shape
-                x2_plus = x2.reshape(b, t * fr)
-                DATA_MEANS = x2_plus.mean(dim=1, keepdim=True)
-                DATA_STD = x2_plus.std(dim=1, keepdim=True)
-                x2_plus = (x2_plus - DATA_MEANS) / DATA_STD
-                X2_list.append(x2_plus.reshape(b, t, fr))
+                data_means = torch.mean(x2, dim=1)
+                data_stds = torch.std(x2, dim=1)
+                x2 = (x2-data_means.unsqueeze(1)) / data_stds.unsqueeze(1)
+                X2_list.append(x2)
 
                 label = torch.as_tensor(np.array(f.get("label"))[0])
                 labels_list.append(label)

models/simclr_transformer.py
@@ -44,8 +44,6 @@ class SimCLR_Transformer(pl.LightningModule):
         cos_sim = cos_sim / self.temperature
         nll = -cos_sim[pos_mask] + torch.logsumexp(cos_sim, dim=-1)
         nll = nll.mean()
-        # Logging loss
-        self.log(mode + '_loss', nll)
         # Get ranking position of positive example
         comb_sim = torch.cat([cos_sim[pos_mask][:, None],  # First position positive example
                               cos_sim.masked_fill(pos_mask, -9e15)],
@@ -81,6 +79,7 @@ class SimCLR_Transformer(pl.LightningModule):
         loss = self.alpha * reconstruction_loss + contrastive_loss
         self.log(mode + "_recon_loss", reconstruction_loss)
         self.log(mode + "_contrastive_loss", contrastive_loss)
+        self.log(mode + "_loss", loss)
         return loss
 
     def training_step(self, batch, batch_idx):